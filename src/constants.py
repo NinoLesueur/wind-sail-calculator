f909d624225dddaf5a6b9f60bbd1906e810a9110
Nino <nino.lesueur@epita.fr>
2026-06-05 20:42:47 +0200

    Add sailing calculation constants

---
+++ src/constants.py
@@ -0,0 +1,17 @@
+LEVEL_FACTOR = {
+    "beginner": 1.4,
+    "intermediate": 1.0,
+    "advanced": 0.7
+}
+
+ACTIVITY_FACTOR = {
+    "windsurf": 1.1,
+    "catamaran": 1.0,
+    "dinghy": 1.2
+}
+
+IDEAL_WIND = {
+    "beginner": (5, 12),
+    "intermediate": (10, 22),
+    "advanced": (15, 30)
+}
